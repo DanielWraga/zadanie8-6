04983dbb0cbafcacbd69390bb1015df2176e3c8a
OmManiPemeHung <danielwraga@gmail.com>
2017-09-02 17:52:42 +0200

    first commit

---
+++ zadanie8-6.js
@@ -0,0 +1,11 @@
+var a = 3,
+	b = 4,
+	value;
+value = (a * a) + (2 * a * b) - (b * b)
+console.log(value);
+alert ('wynik=' + value);
+if (value >= 0) {
+alert ('wynik dodatni')
+} else { (value = 0) 
+alert ('wynik = 0')
+}
